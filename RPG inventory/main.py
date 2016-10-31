158054c6962f24f384ac69d5f17272f1aa7c2842
Telakos <tel.akos.2@gmail.com>
2016-10-31 16:54:03 +0100

    step1 - done step2 - idk why it fails

--- RPG inventory/main.py
+++ RPG inventory/main.py
@@ -1,12 +1,26 @@
 inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 
 
 
 
 def display_inventory(action):
-    if action == "y":
-        print(inv)
+    for k, v  in inv.items():
+        print(v," ", k)
+
+def add_to_inventory(inventory,added_items):
+    ni = dragon_loot
+    for i in dragon_loot:
+        if KeyError:
+            inv[i] += 1
+        else:
+            inv.update({i:1})
+       
+        
+
+    
+        
 
 
 
@@ -14,4 +28,10 @@ def display_inventory(action):
 
 
 while True:
-   action = (input("do you want to check your inventory?: "))
+    action = "A" #(input("what do you want to do? : "))
+    #if action == "C":
+     #   display_inventory(action)
+    if action == "A":
+        add_to_inventory(inv, dragon_loot)
+    elif action == "t":
+        print(inv['gold coin'])
