b287b70c2087a4509cbcd8370c7f90c44340c3f3
Telakos <tel.akos.2@gmail.com>
2016-11-01 14:52:11 +0100

    fileimport starting to get in shape... finally

--- RPG inventory/save_load.py
+++ RPG inventory/save_load.py
@@ -1,14 +1,12 @@
 import csv
 
-
 def load_inventory(saveFile):
     impinv = {}
     row = 0
     file = open(saveFile, "r")
-    reader = csv.reader(file, quoting = csv.QUOTE_NONE)
-    impinv = dict(filter(None, csv.reader(file)))
-    #impinv=dict((row[0], row[1])for row in reader)
-
-
-    print(impinv)
+    reader = csv.reader(file, quoting = csv.QUOTE_MINIMAL, doublequote = False)
+    #impinv = dict(filter(None, csv.reader(file, quoting = csv.QUOTE_NONE)))
+    impinv=dict((row[0], row[1])for row in reader)
+    for k, v in impinv.items():
+        print(v, " ", k)
     file.close()
