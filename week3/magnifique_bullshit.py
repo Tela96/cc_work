9b28c44e7ce302799bf766a13e6bc40f35f13ba5
Tela96 <tel.akos.2@gmail.com>
2017-01-24 14:46:39 +0100

    removed prolly vulgar content

--- week3/magnifique_bullshit.py
+++
@@ -1,8 +0,0 @@
-import random
-
-lista = [1, 2, 3, 45, 6, 7, 8, 2110, 10000]
-valtozo = 0
-for i in range(len(lista)):
-    while valtozo != lista[i]:
-        valtozo = random.randint(1, 10000)
-        print(valtozo)
